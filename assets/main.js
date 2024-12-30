35d48361787a5ea66624965f07b14a9d4e864055
HMH6868 <huaminhhoang9999@gmail.com>
2024-12-30 21:19:24 +0700

    fomat

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,7 @@ const images = Array.from(document.querySelectorAll('.carousel img'));
 const prevButton = document.querySelector('.carousel-button.prev');
 const nextButton = document.querySelector('.carousel-button.next');
 
-let currentIndex = 0; 
+let currentIndex = 0;
 
 function updateCarousel() {
     const orderedImages = [
@@ -26,7 +26,7 @@ prevButton.addEventListener('click', () => {
 
 // Xử lý nút next
 nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % images.length; 
+    currentIndex = (currentIndex + 1) % images.length;
     updateCarousel();
 });
 
@@ -37,4 +37,42 @@ updateCarousel();
 window.onload = function() {
     var element = document.getElementById('contact');
     element.scrollIntoView();
-  };
+};
+
+
+const form = document.getElementById('myForm');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Vô hiệu hóa nút submit
+    const submitButton = document.querySelector('button[type="submit"]');
+    submitButton.disabled = true;
+
+    const fullname = document.getElementById('fullname').value;
+    const phone = document.getElementById('phone').value;
+    const province = document.getElementById('province').value;
+    const message = document.getElementById('message').value;
+
+    fetch('https://script.google.com/macros/s/AKfycbzazkxh0VJRJQfxS-LyyBb_3Mqo_bQNamgc54_VXRP-5kd-UbRh5D3KAwvqJLAuJV4I/exec', {
+            method: 'POST',
+            body: new URLSearchParams({
+                fullname: fullname,
+                phone: phone,
+                province: province,
+                message: message
+            })
+        })
+        .then(response => response.text())
+        .then(data => {
+            alert(data);
+            form.reset();
+            // Bật lại nút submit sau khi lưu thành công
+            submitButton.disabled = false;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Đã xảy ra lỗi!');
+            // Bật lại nút submit nếu có lỗi
+            submitButton.disabled = false;
+        });
+});
