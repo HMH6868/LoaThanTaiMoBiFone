9f26fe759137836badd53d479dc07e638b15e2e4
thanhnam1122 <117763613+thanhnam1122@users.noreply.github.com>
2025-01-01 11:42:22 +0700

    update

--- assets/main.js
+++ assets/main.js
@@ -1,36 +1,36 @@
-const carousel = document.querySelector('.carousel-container');
-const images = Array.from(document.querySelectorAll('.carousel img'));
-const prevButton = document.querySelector('.carousel-button.prev');
-const nextButton = document.querySelector('.carousel-button.next');
+// const carousel = document.querySelector('.carousel-container');
+// const images = Array.from(document.querySelectorAll('.carousel img'));
+// const prevButton = document.querySelector('.carousel-button.prev');
+// const nextButton = document.querySelector('.carousel-button.next');
 
-let currentIndex = 0;
+// let currentIndex = 0;
 
-function updateCarousel() {
-    const orderedImages = [
-        images[currentIndex],
-        images[(currentIndex + 1) % images.length],
-        images[(currentIndex + 2) % images.length],
-    ];
+// function updateCarousel() {
+//     const orderedImages = [
+//         images[currentIndex],
+//         images[(currentIndex + 1) % images.length],
+//         images[(currentIndex + 2) % images.length],
+//     ];
 
-    carousel.innerHTML = '';
-    orderedImages.forEach(image => {
-        carousel.appendChild(image);
-    });
-}
+//     carousel.innerHTML = '';
+//     orderedImages.forEach(image => {
+//         carousel.appendChild(image);
+//     });
+// }
 
-// Xử lý nút prev
-prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    updateCarousel();
-});
+// // Xử lý nút prev
+// prevButton.addEventListener('click', () => {
+//     currentIndex = (currentIndex - 1 + images.length) % images.length;
+//     updateCarousel();
+// });
 
-// Xử lý nút next
-nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % images.length;
-    updateCarousel();
-});
+// // Xử lý nút next
+// nextButton.addEventListener('click', () => {
+//     currentIndex = (currentIndex + 1) % images.length;
+//     updateCarousel();
+// });
 
-updateCarousel();
+// updateCarousel();
 
 
 
@@ -43,7 +43,6 @@ window.onload = function() {
 const form = document.getElementById('myForm');
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-
     // Vô hiệu hóa nút submit
     const submitButton = document.querySelector('button[type="submit"]');
     submitButton.disabled = true;
